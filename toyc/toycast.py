52510108e7a863c5da226a664d50e81ce69f1c36
Liam Eckert <liameckert17@gmail.com>
2025-06-23 01:18:09 -0400

    Froze toy c ast nodes

--- toyc/toycast.py
+++ toyc/toycast.py
@@ -4,22 +4,22 @@ from abc import ABC
 from typing import Iterator, List, Optional, Union
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class AstNode(ABC):
     line: int
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Stmt(AstNode):
     pass
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Expr(AstNode):
     pass
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Body(AstNode):
     statements: List[Stmt]
 
@@ -27,13 +27,13 @@ class Body(AstNode):
         return iter(self.statements)
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Param(AstNode):
     name: str
     type: str
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class FnDecl(AstNode):
     name: str
     params: List[Param]
@@ -42,12 +42,12 @@ class FnDecl(AstNode):
 
 
 @dataclass(slots=True)
-class Program(AstNode):
+class Program:
     name: str
     functions: List[FnDecl]
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class VarDecl(Stmt):
     mutable: bool
     type: str
@@ -55,72 +55,72 @@ class VarDecl(Stmt):
     value: Expr
 
 
-@dataclass(slots=True)
-class Assign(Expr, Stmt):
+@dataclass(slots=True, frozen=True)
+class Assign(Stmt, Expr):
     left: Expr
     right: Expr
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class If(Stmt):
     condition: Expr
     then_branch: Body
     else_branch: Optional[Union[Body, "If"]]
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True  )
 class Goto(Stmt):
     label: str
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Label(Stmt):
     label: str
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Return(Stmt):
     value: Optional[Expr]
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class ExprStmt(Stmt):
     expr: Expr
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Call(Expr):
     callee: Expr
     args: List[Expr]
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class IntLit(Expr):
     value: int
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class FloatLit(Expr):
     value: float
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class BoolLit(Expr):
     value: bool
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Ident(Expr):
     name: str
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Unary(Expr):
     op: str
     operand: Expr
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class Binary(Expr):
     left: Expr
     op: str
